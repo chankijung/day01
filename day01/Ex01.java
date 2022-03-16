90985773b24b61291072dce13ad21cc023b94766
chanki <chanki9507@gmail.com>
2022-03-16 11:33:20 +0900

    Ex01 print

---
+++ day01/Ex01.java
@@ -0,0 +1,17 @@
+package day01;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		System.out.println("안녕하세요~!");
+		System.out.println("안녕하세요~!");
+		System.out.print("추우");
+		System.out.print("울력");
+		//출력하는 기능
+		
+		/*
+		여러줄 주석
+		으로 묶을 때
+		쓰는 문단 주석
+		*/
+	}
+}
