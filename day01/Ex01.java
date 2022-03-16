3ba3baf17d9bc6fee652d39eae1fd466256ef10f
chanki <chanki9507@gmail.com>
2022-03-16 14:20:31 +0900

    이스케이프활용

--- day01/Ex01.java
+++ day01/Ex01.java
@@ -17,6 +17,17 @@ public class Ex01 {
 		System.out.println("학원에서 추가");
 		System.out.println("집에서 추가");
 		System.out.println("집에서 한번 더 해봄");
-
+		
+		/* 	\n : 엔터
+		 	\t : tab 크기만큼 커서를 오른쪽으로 이동(스페이스 8칸)
+		 	\" : "(큰 따옴표) 표현
+		 	\\ : \ 하나 표현
+		*/
+		System.out.println("안\n녕하세\n요\n");
+		System.out.println("\t역시나 맞네 \t123\t12345");
+		System.out.println("\"밥상머리에서 !!\"");
+		System.out.println("\\소곤소곤\\");
+		System.out.println("C:\\자바 취업반\\새 폴더");
 	}
+	
 }
---
+++ day01/Ex02.java
@@ -0,0 +1,22 @@
+package day01;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		System.out.println(100 + 200);
+		System.out.println(10.13 + 1.221);
+		// 큰따옴표 없이 숫자로만 써지면 숫자로 인식
+		// ;는 세미콜론. 마침표로 이해
+		
+		System.out.println("백"+"\t"+100+"\n"+100);
+		// 문자열과 숫자를 합치면 숫자도 문자열로 인식
+		// 개행문자 = 이스케이프문자는 쌍따옴표 안에 있어야 함
+		
+		System.out.println("백"+"백");
+		System.out.println("100"+"100");
+		//문자에서 +는 문자를 이어써주는 기능
+		
+		
+		
+	}
+
+}
